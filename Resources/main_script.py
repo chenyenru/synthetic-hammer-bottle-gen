236de98c4fca3e9ec780d54a26b058fbf2388815
ChenYenRu <rebecca946487@gmail.com>
2024-02-18 15:19:12 -0800

    remove the set_hammer() function since it does relative rotation

--- Resources/main_script.py
+++ Resources/main_script.py
@@ -22,9 +22,7 @@ class Render:
         self.axis = bpy.data.objects["Main Axis"]
         self.light_1 = bpy.data.objects["Light1"]
         self.light_2 = bpy.data.objects["Light2"]
-        self.obj_names = [
-            "Hammer",
-        ]
+        self.obj_names = ["Hammer", "Bottle"]
         self.bg = bpy.data.objects["Plane"]
         self.objects = (
             self.create_objects()
@@ -36,22 +34,13 @@ class Render:
         # γ (or ψ) represents a rotation around the z″ axis.
 
         # Define range of heights z in m that the camera is going to pan through
-        self.camera_d_limits = [
-            0.2,
-            1.2,
-        ]
+        self.camera_d_limits = [0.6, 0.6]
 
         # Define range of beta angles that the camera is going to pan through
-        self.beta_limits = [
-            50,
-            -50,
-        ]
+        self.beta_limits = [50, -50]
 
         # Define range of gamma angles that the camera is going to pan through
-        self.gamma_limits = [
-            0,
-            360,
-        ]
+        self.gamma_limits = [0, 30]
 
         ## Output information
         # Input your own preferred location for the images and labels
@@ -65,14 +54,15 @@ class Render:
         self.axis.rotation_euler = (0, 0, 0)
         self.axis.location = (0, 0, 0)
         self.camera.location = (0, 0, 1.2)
+        self.camera.scale = (1, 1, 0.2)
 
-    def set_hammer(self):
-        """
-        Set Hammer Information
-        """
-        self.objects[0].rotation_euler = (90, 0, 180)
-        self.objects[0].location = (0, -0.125, 0)
-        self.objects[0].scale = (0.01, 0.01, 0.01)
+    # def set_hammer(self):
+    #     """
+    #     Set Hammer Information
+    #     """
+    #     self.objects[0].rotation_euler = (90, 0, 180)
+    #     self.objects[0].location = (0, -0.125, 0)
+    #     self.objects[0].scale = (0.01, 0.01, 0.01)
 
     def main_rendering_loop(self, rot_step, pathname, bg_counter):
         """
@@ -83,7 +73,7 @@ class Render:
         n_renders = self.calculate_n_renders(rot_step)  # Calculate number of images
         print("Number of renders to create:", n_renders)
 
-        self.new_bg_material(pathname)
+        # self.new_bg_material(pathname)
 
         accept_render = input(
             "\nContinue?[Y/N]:  "
@@ -93,108 +83,104 @@ class Render:
             accept_render == "Y"
         ):  # If the user inputs 'Y' then procede with the data generation
             # Create .txt file that record the progress of the data generation
-            report_file_path = self.labels_filepath + "/progress_report.txt"
-            report = open(report_file_path, "w")
-            # Multiply the limits by 10 to adapt to the for loop
-            dmin = int(self.camera_d_limits[0] * 10)
-            dmax = int(self.camera_d_limits[1] * 10)
-            # Define a counter to name each .png and .txt files that are outputted
-            render_counter = 0
-            # Define the step with which the pictures are going to be taken
-            rotation_step = rot_step
+            report_file_path = f"{self.labels_filepath}/progress_report.txt"
+            with open(report_file_path, "w") as report:
+                dmax = int(self.camera_d_limits[1] * 10)
+                # Define a counter to name each .png and .txt files that are outputted
+                render_counter = 0
+                # Define the step with which the pictures are going to be taken
+                rotation_step = rot_step
 
-            # Begin nested loops
-            for d in range(dmin, dmax + 1, 2):  # Loop to vary the height of the camera
-                ## Update the height of the camera
-                self.camera.location = (
-                    0,
-                    0,
-                    # d / 10,
-                    d,
-                )  # Divide the distance z by 10 to re-factor current height
+                dmin = int(self.camera_d_limits[0] * 10)
+                # Begin nested loops
+                for d in range(
+                    dmin, dmax + 1, 10
+                ):  # Loop to vary the height of the camera
+                    ## Update the height of the camera
+                    self.camera.location = (
+                        0,
+                        0,
+                        d / 10,
+                        # d,
+                    )  # Divide the distance z by 10 to re-factor current height
 
-                # Refactor the beta limits for them to be in a range from 0 to 360 to adapt the limits to the for loop
-                min_beta = (-1) * self.beta_limits[0] + 90
-                max_beta = (-1) * self.beta_limits[1] + 90
+                    # Refactor the beta limits for them to be in a range from 0 to 360 to adapt the limits to the for loop
+                    min_beta = (-1) * self.beta_limits[0] + 90
+                    max_beta = (-1) * self.beta_limits[1] + 90
 
-                for beta in range(
-                    min_beta, max_beta + 1, rotation_step
-                ):  # Loop to vary the angle beta
-                    beta_r = (-1) * beta + 90  # Re-factor the current beta
+                    for beta in range(
+                        min_beta, max_beta + 1, rotation_step
+                    ):  # Loop to vary the angle beta
+                        beta_r = (-1) * beta + 90  # Re-factor the current beta
 
-                    for gamma in range(
-                        self.gamma_limits[0], self.gamma_limits[1] + 1, rotation_step
-                    ):  # Loop to vary the angle gamma
-                        render_counter += 1  # Update counter
+                        for gamma in range(
+                            self.gamma_limits[0],
+                            self.gamma_limits[1] + 1,
+                            rotation_step,
+                        ):  # Loop to vary the angle gamma
+                            render_counter += 1  # Update counter
 
-                        ## Update the rotation of the axis
-                        axis_rotation = (m.radians(beta_r), 0, m.radians(gamma))
-                        self.axis.rotation_euler = axis_rotation  # Assign rotation to <bpy.data.objects['Empty']> object
-                        # Display demo information - Location of the camera
-                        print("On render:", render_counter)
-                        print("--> Location of the camera:")
-                        print("     d:", d, "m")
-                        print("     Beta:", str(beta_r) + "Degrees")
-                        print("     Gamma:", str(gamma) + "Degrees")
+                            ## Update the rotation of the axis
+                            axis_rotation = (m.radians(beta_r), 0, m.radians(gamma))
+                            self.axis.rotation_euler = axis_rotation  # Assign rotation to <bpy.data.objects['Empty']> object
+                            # Display demo information - Location of the camera
+                            print("On render:", render_counter)
+                            print("--> Location of the camera:")
+                            print("     d:", d, "m")
+                            print("     Beta:", f"{str(beta_r)}Degrees")
+                            print("     Gamma:", f"{str(gamma)}Degrees")
 
-                        ## Configure lighting
-                        energy1 = random.randint(0, 30)  # Grab random light intensity
-                        self.light_1.data.energy = energy1  # Update the <bpy.data.objects['Light']> energy information
-                        energy2 = random.randint(4, 20)  # Grab random light intensity
-                        self.light_2.data.energy = energy2  # Update the <bpy.data.objects['Light2']> energy information
+                            ## Configure lighting
+                            energy1 = random.randint(
+                                0, 30
+                            )  # Grab random light intensity
+                            self.light_1.data.energy = energy1  # Update the <bpy.data.objects['Light']> energy information
+                            energy2 = random.randint(
+                                4, 20
+                            )  # Grab random light intensity
+                            self.light_2.data.energy = energy2  # Update the <bpy.data.objects['Light2']> energy information
 
-                        ## Generate render
-                        self.render_blender(
-                            f"{render_counter}_{bg_counter}"
-                        )  # Take photo of current scene and ouput the render_counter.png file
-                        # Display demo information - Photo information
-                        print("--> Picture information:")
-                        print(
-                            "     Resolution:",
-                            (self.xpix * self.percentage, self.ypix * self.percentage),
-                        )
-                        print("     Rendering samples:", self.samples)
+                            ## Generate render
+                            self.render_blender(
+                                f"{render_counter}_{bg_counter}"
+                            )  # Take photo of current scene and ouput the render_counter.png file
+                            # Display demo information - Photo information
+                            print("--> Picture information:")
+                            print(
+                                "     Resolution:",
+                                (
+                                    self.xpix * self.percentage,
+                                    self.ypix * self.percentage,
+                                ),
+                            )
+                            print("     Rendering samples:", self.samples)
 
-                        ## Output Labels
-                        text_file_name = (
-                            self.labels_filepath
-                            + "/"
-                            + str(render_counter)
-                            + f"_{bg_counter}"
-                            + ".txt"
-                        )  # Create label file name
-                        text_file = open(
-                            text_file_name, "w+"
-                        )  # Open .txt file of the label
-                        # Get formatted coordinates of the bounding boxes of all the objects in the scene
-                        # Display demo information - Label construction
-                        print("---> Label Construction")
-                        text_coordinates = self.get_all_coordinates()
-                        splitted_coordinates = text_coordinates.split("\n")[
-                            :-1
-                        ]  # Delete last '\n' in coordinates
-                        text_file.write(
-                            "\n".join(splitted_coordinates)
-                        )  # Write the coordinates to the text file and output the render_counter.txt file
-                        text_file.close()  # Close the .txt file corresponding to the label
-
-                        ## Show progress on batch of renders
-                        print("Progress =", str(render_counter) + "/" + str(n_renders))
-                        report.write(
-                            "Progress: "
-                            + str(render_counter)
-                            + " Rotation: "
-                            + str(axis_rotation)
-                            + " z_d: "
-                            + str(d / 10)
-                            + "\n"
-                        )
-
-            report.close()  # Close the .txt file corresponding to the report
+                            ## Output Labels
+                            text_file_name = (
+                                f"{self.labels_filepath}/{render_counter}"
+                                + f"_{bg_counter}"
+                                + ".txt"
+                            )
+                            with open(text_file_name, "w+") as text_file:
+                                # Get formatted coordinates of the bounding boxes of all the objects in the scene
+                                # Display demo information - Label construction
+                                print("---> Label Construction")
+                                text_coordinates = self.get_all_coordinates()
+                                splitted_coordinates = text_coordinates.split("\n")[
+                                    :-1
+                                ]  # Delete last '\n' in coordinates
+                                text_file.write(
+                                    "\n".join(splitted_coordinates)
+                                )  # Write the coordinates to the text file and output the render_counter.txt file
+                                ## Show progress on batch of renders
+                            print("Progress =", f"{render_counter}/{str(n_renders)}")
+                            report.write(
+                                f"Progress: {render_counter} Rotation: {axis_rotation} z_d: {str(d / 10)}"
+                                + "\n"
+                            )
 
         else:  # If the user inputs anything else, then abort the data generation
             print("Aborted rendering operation")
-            pass
 
     def get_all_coordinates(self):
         """
@@ -206,8 +192,7 @@ class Render:
         )
         for i, objct in enumerate(self.objects):  # Loop through all of the objects
             print("     On object:", objct)
-            b_box = self.find_bounding_box(objct)  # Get current object's coordinates
-            if b_box:  # If find_bounding_box() doesn't return None
+            if b_box := self.find_bounding_box(objct):
                 print("         Initial coordinates:", b_box)
                 text_coordinates = self.format_coordinates(
                     b_box, i
@@ -216,11 +201,8 @@ class Render:
                 main_text_coordinates = (
                     main_text_coordinates + text_coordinates
                 )  # Update main_text_coordinates variables whith each
-                # line corresponding to each class in the frame of the current image
             else:
                 print("         Object not visible")
-                pass
-
         return main_text_coordinates  # Return all coordinates
 
     def format_coordinates(self, coordinates, classe):
@@ -229,38 +211,22 @@ class Render:
         the image width and the image height and outputs the coordinates of the bounding box of the current class
         """
         # If the current class is in view of the camera
-        if coordinates:
-            ## Change coordinates reference frame
-            x1 = coordinates[0][0]
-            x2 = coordinates[1][0]
-            y1 = 1 - coordinates[1][1]
-            y2 = 1 - coordinates[0][1]
+        if not coordinates:
+            return
+        ## Change coordinates reference frame
+        x1 = coordinates[0][0]
+        x2 = coordinates[1][0]
+        y1 = 1 - coordinates[1][1]
+        y2 = 1 - coordinates[0][1]
 
-            ## Get final bounding box information
-            width = x2 - x1  # Calculate the absolute width of the bounding box
-            height = y2 - y1  # Calculate the absolute height of the bounding box
-            # Calculate the absolute center of the bounding box
-            cx = x1 + (width / 2)
-            cy = y1 + (height / 2)
+        ## Get final bounding box information
+        width = x2 - x1  # Calculate the absolute width of the bounding box
+        height = y2 - y1  # Calculate the absolute height of the bounding box
+        # Calculate the absolute center of the bounding box
+        cx = x1 + (width / 2)
+        cy = y1 + (height / 2)
 
-            ## Formulate line corresponding to the bounding box of one class
-            txt_coordinates = (
-                str(classe)
-                + " "
-                + str(cx)
-                + " "
-                + str(cy)
-                + " "
-                + str(width)
-                + " "
-                + str(height)
-                + "\n"
-            )
-
-            return txt_coordinates
-        # If the current class isn't in view of the camera, then pass
-        else:
-            pass
+        return f"{str(classe)} {str(cx)} {str(cy)} {str(width)} {str(height)}" + "\n"
 
     def find_bounding_box(self, obj):
         """
@@ -337,6 +303,13 @@ class Render:
         return (min_x, min_y), (max_x, max_y)
 
     def render_blender(self, count_f_name):
+        """
+        Renders an image using random parameters and exports it.
+
+        Args:
+            count_f_name (str): The name of the file to save the rendered image.
+
+        """
         # Define random parameters
         random.seed(random.randint(1, 1000))
         self.xpix = random.randint(500, 1000)
@@ -344,7 +317,7 @@ class Render:
         self.percentage = random.randint(90, 100)
         self.samples = random.randint(25, 50)
         # Render images
-        image_name = str(count_f_name) + ".png"
+        image_name = f"{str(count_f_name)}.png"
         self.export_render(
             self.xpix,
             self.ypix,
@@ -360,12 +333,21 @@ class Render:
         self.scene.render.resolution_x = res_x
         self.scene.render.resolution_y = res_y
         self.scene.render.resolution_percentage = res_per
-        self.scene.render.filepath = file_path + "/" + file_name
+        self.scene.render.filepath = f"{file_path}/{file_name}"
 
         # Take picture of current visible scene
         bpy.ops.render.render(write_still=True)
 
     def calculate_n_renders(self, rotation_step):
+        """
+        Calculates the number of renders based on the given rotation step.
+
+        Args:
+            rotation_step (int): The step size for rotation angles.
+
+        Returns:
+            int: The total number of renders.
+        """
         zmin = int(self.camera_d_limits[0] * 10)
         zmax = int(self.camera_d_limits[1] * 10)
 
@@ -391,11 +373,7 @@ class Render:
     def create_objects(
         self,
     ):  # This function creates a list of all the <bpy.data.objects>
-        objs = []
-        for obj in self.obj_names:
-            objs.append(bpy.data.objects[obj])
-
-        return objs
+        return [bpy.data.objects[obj] for obj in self.obj_names]
 
     def new_bg_material(self, pathname):
         material = bpy.data.materials.new(name="generated_material")
@@ -453,12 +431,13 @@ if __name__ == "__main__":
     r.set_camera()
 
     # Set first object/hammer's information
-    r.set_hammer()
+    # r.set_hammer()
     # Begin data generation
     rotation_step = 5
 
+    # Different texture paths
     texture_path = "/Users/chen_yenru/Documents/GitHub/SCHOOL/UCSD/YonderDynamics/synthetic-hammer-bottle-gen/Resources/Blender_Files/Textures"
     texture_paths_list = get_absolute_paths(texture_path)
 
-    for i in range(len(texture_paths_list)):
-        r.main_rendering_loop(rotation_step, texture_paths_list[i], i)
+    # for i in range(len(texture_paths_list)):
+    r.main_rendering_loop(rotation_step, texture_paths_list[0], 1)
